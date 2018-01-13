8e5ddd36c4cd6509b5c1a9622b185f12a50bb320
Reagan Bourne <reagan.bourne@icloud.com>
2018-01-13 15:03:18 -0500

    design Song model

--- sortify/models.py
+++ sortify/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Song(models.Model):
+    spotify_id = models.CharField(null=False)
+    acousticness = models.FloatField()
+    analysis_url = models.URLField()
+    danceability = models.FloatField()
+    duration_ms = models.IntegerField()
+    energy = models.FloatField()
+    instrumentalness = models.FloatField()
+    key = models.IntegerField()
+    liveness = models.FloatField()
+    loudness = models.FloatField()
+    mode = models.IntegerField()
+    speechiness = models.FloatField()
+    tempo = models.FloatField()
+    time_signature = models.IntegerField()
+    track_href = models.URLField()
+    valence = models.FloatField()
